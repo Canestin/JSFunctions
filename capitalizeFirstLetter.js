c8c2409e58f409f65087c2c4703a6f9ab5e050f6
Canestin <canestinndong@MacBook-Pro-de-Canestin.local>
2022-10-05 21:38:43 +0200

    feat: add capitalizeFirstLetter function

---
+++ capitalizeFirstLetter.js
@@ -0,0 +1,15 @@
+/** This function allows you to make the first letter of a word capitalized.
+ * Like the title() function in Python
+ */
+function capitalizeFirstLetter(sentence) {
+  let arraySentence = sentence.split(" ");
+
+  for (let i = 0; i < arraySentence.length; i++) {
+    const word = arraySentence[i];
+    const firstLetter = word.charAt(0);
+    arraySentence[i] = word.replace(firstLetter, firstLetter.toUpperCase());
+  }
+  // Here, replace allows to replace wide white spaces with simple white spaces
+  const newSentence = arraySentence.join(" ").replace(/(\s){2,}/g, " ");
+  console.log(newSentence);
+}
